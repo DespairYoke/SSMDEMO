3b06bc022995a45fb071f60146535ed038a22b09
love <810095178@qq.com>
2017-11-30 00:59:47 +0800

    dao、entity和数据连接的添加

---
+++ src/main/java/dao/AuthorDao.java
@@ -0,0 +1,13 @@
+package dao;
+
+import entity.Author;
+
+import java.util.List;
+
+public interface AuthorDao {
+    int add(Author author);
+    int update(Author author);
+    int delete(Long id);
+    Author findAuthor(Long id);
+    List<Author> findAuthorList();
+}
